9e08ff4891235b89b5b89d316c5764e79c7bf1ea
Muratcan Baba <muratcanbaba1@gmail.com>
2022-03-30 23:02:18 +0300

    Drag cards and drop lists feature added

--- src/components/BoardDetailsPageComponents/CardBoxComponents/CardBox.tsx
+++ src/components/BoardDetailsPageComponents/CardBoxComponents/CardBox.tsx
@@ -2,22 +2,31 @@ import { Box, Text, Icon } from '@chakra-ui/react';
 import { BiCommentDetail, BiFile } from 'react-icons/bi';
 import CardMiniLabel from './CardMiniLabel';
 import CardCheckTag from './CardCheckTag';
-import { FC, useState } from 'react';
-import { useAppSelector } from '../../../store';
+import { FC, useEffect, useState } from 'react';
+import { useAppDispatch, useAppSelector } from '../../../store';
 import CardModal from '../CardModalComponents/CardModal';
 import React from 'react';
 import CardDateTag from './CardDateTag';
+import { Draggable } from 'react-beautiful-dnd';
+import authRequest from '../../../service/authRequest';
+import { fetchBoard } from '../../../features/boardSlice';
+import { useParams } from 'react-router-dom';
 
 interface CardBoxProps {
   cardId: number;
   listId: number;
   listName: string;
   boardName: string;
-  ref?: any;
+  index?: number;
+  cardOrder: number;
 }
 
-const CardBox: FC<CardBoxProps> = ({ cardId, listId, listName, boardName }) => {
+const CardBox: FC<CardBoxProps> = ({ cardId, listId, listName, boardName, index, cardOrder }) => {
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+
+  const { id } = useParams();
+
+  const dispatch = useAppDispatch();
 
   const onOpen = () => {
     setIsModalOpen(!isModalOpen);
@@ -34,6 +43,14 @@ const CardBox: FC<CardBoxProps> = ({ cardId, listId, listName, boardName }) => {
       .find((listItem) => listItem.id === listId)
       ?.cards.find((cardItem) => cardItem.id === cardId)
   );
+
+  useEffect(() => {
+    authRequest()
+      .put(`card/${cardId}`, { order: index })
+      .then(() => {
+        dispatch(fetchBoard(id!));
+      });
+  }, [cardId, dispatch, id, index]);
 
   const calculateTotalChecks = () => {
     let totalChecks = 0;
@@ -56,55 +73,69 @@ const CardBox: FC<CardBoxProps> = ({ cardId, listId, listName, boardName }) => {
   };
 
   return (
-    <Box
-      bg="rgb(250,250,250)"
-      _hover={{ cursor: 'pointer' }}
-      onClick={onOpen}
-      ref={divRef}
-      borderRadius="2xl"
-      boxShadow="sm"
-      width={286}
-      border="1px"
-      borderColor="rgb(230,230,230)"
-    >
-      <Box p="15px">
-        <Box display="flex" gap={2}>
-          {card?.labels.map((item) => {
-            return <CardMiniLabel labelId={item.id} key={item.id} />;
-          })}
-        </Box>
-        <Text width={'90%'} overflow="hidden" textOverflow={'ellipsis'} whiteSpace="nowrap" my="18px">
-          {card?.title}
-        </Text>
-        <Box display="flex" justifyContent="start" gap="5px">
-          {card?.duedate && card?.duedate !== '2010-01-01' && <CardDateTag dueDate={card?.duedate} />}
-          {card?.checklists.length !== 0 && (
-            <CardCheckTag totalCheckeds={() => calculateTotalCheckeds()} totalChecks={() => calculateTotalChecks()} />
-          )}
-        </Box>
-      </Box>
-
-      <Box
-        display="flex"
-        alignItems="center"
-        justifyContent="space-between"
-        height="47px"
-        p="18px"
-        borderTop="1px"
-        borderColor="rgb(230,230,230)"
-      >
-        <Box>{card?.description && <Icon boxSize={4} as={BiFile} />}</Box>
-        {card?.comments.length !== 0 && (
-          <Box display="flex" justifyContent="space-between" alignItems="center" width="30px">
-            <Icon boxSize={4} mt="2px" as={BiCommentDetail} />
-            <Text fontSize="xs" display="inline-block" lineHeight="0">
-              {card?.comments.length}
-            </Text>
+    <Draggable draggableId={card?.id.toString()!} index={cardOrder}>
+      {(provided) => (
+        <Box {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef}>
+          <Box
+            bg="rgb(250,250,250)"
+            _hover={{ cursor: 'pointer' }}
+            onClick={onOpen}
+            ref={divRef}
+            borderRadius="2xl"
+            boxShadow="sm"
+            width={286}
+            border="1px"
+            borderColor="rgb(230,230,230)"
+          >
+            <Box p="15px">
+              <Box display="flex" gap={2}>
+                {card?.labels.map((item) => {
+                  return <CardMiniLabel labelId={item.id} key={item.id} />;
+                })}
+              </Box>
+              <Text width={'90%'} overflow="hidden" textOverflow={'ellipsis'} whiteSpace="nowrap" my="18px">
+                {card?.title}
+              </Text>
+              <Box display="flex" justifyContent="start" gap="5px">
+                {card?.duedate && card?.duedate !== '2010-01-01' && <CardDateTag dueDate={card?.duedate} />}
+                {card?.checklists.length !== 0 && (
+                  <CardCheckTag
+                    totalCheckeds={() => calculateTotalCheckeds()}
+                    totalChecks={() => calculateTotalChecks()}
+                  />
+                )}
+              </Box>
+            </Box>
+            <Box
+              display="flex"
+              alignItems="center"
+              justifyContent="space-between"
+              height="47px"
+              p="18px"
+              borderTop="1px"
+              borderColor="rgb(230,230,230)"
+            >
+              <Box>{card?.description && <Icon boxSize={4} as={BiFile} />}</Box>
+              {card?.comments.length !== 0 && (
+                <Box display="flex" justifyContent="space-between" alignItems="center" width="30px">
+                  <Icon boxSize={4} mt="2px" as={BiCommentDetail} />
+                  <Text fontSize="xs" display="inline-block" lineHeight="0">
+                    {card?.comments.length}
+                  </Text>
+                </Box>
+              )}
+            </Box>
+            <CardModal
+              card={card!}
+              openModal={isModalOpen}
+              handleClose={onClose}
+              listName={listName}
+              boardName={boardName}
+            />
           </Box>
-        )}
-      </Box>
-      <CardModal card={card!} openModal={isModalOpen} handleClose={onClose} listName={listName} boardName={boardName} />
-    </Box>
+        </Box>
+      )}
+    </Draggable>
   );
 };
 
--- src/components/BoardDetailsPageComponents/ListBox.tsx
+++ src/components/BoardDetailsPageComponents/ListBox.tsx
@@ -1,6 +1,10 @@
 import { Box } from '@chakra-ui/react';
-import { FC } from 'react';
-import { useAppSelector } from '../../store';
+import { FC, useEffect } from 'react';
+import { Draggable, Droppable } from 'react-beautiful-dnd';
+import { useParams } from 'react-router-dom';
+import { fetchBoard } from '../../features/boardSlice';
+import authRequest from '../../service/authRequest';
+import { useAppDispatch, useAppSelector } from '../../store';
 import CardBox from './CardBoxComponents/CardBox';
 import EditableAddButton from './EditableAddButton';
 import NameSettingsHolder from './NameSettingsHolder';
@@ -9,51 +13,92 @@ interface ListBoxProps {
   listId: number;
   listName: string;
   boardName: string;
+  index?: number;
+  listOrder: number;
 }
 
-const ListBox: FC<ListBoxProps> = ({ listId, listName, boardName }) => {
+const ListBox: FC<ListBoxProps> = ({ listId, listName, boardName, index, listOrder }) => {
   const list = useAppSelector((state) => state.boardState.data?.lists.find((item) => item.id === listId));
+  const { id } = useParams();
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    authRequest()
+      .put(`list/${listId}`, { order: index })
+      .then(() => {
+        dispatch(fetchBoard(id!));
+      });
+  }, [listId, dispatch, id, index]);
 
   return (
-    <Box
-      bg="rgb(245,245,245)"
-      borderRadius="2xl"
-      boxShadow="sm"
-      width={320}
-      border="1px"
-      borderColor="rgb(230,230,230)"
-    >
-      <Box pb="15px" width={320}>
-        <NameSettingsHolder subCompName="list" listId={listId} listName={listName} />
-      </Box>
-      <Box
-        minHeight="30px"
-        maxHeight="360px"
-        overflowY="auto"
-        sx={{
-          '&::-webkit-scrollbar': {
-            width: '5px',
-            borderRadius: '8px',
-            backgroundColor: `rgba(0, 0, 0, 0.05)`,
-          },
-          '&::-webkit-scrollbar-thumb': {
-            borderRadius: '8px',
-            backgroundColor: `rgb(179,179,179)`,
-          },
-        }}
-      >
-        <Box display="flex" flexDirection="column" justifyContent="center" alignItems="center" gap="20px" py="20px">
-          {list?.cards?.map((item) => {
-            return (
-              <CardBox key={item.id} cardId={item.id} listId={list.id} listName={list.title} boardName={boardName} />
-            );
-          })}
+    <Draggable draggableId={list?.id.toString()!} index={index ? index : 0}>
+      {(provided) => (
+        <Box
+          {...provided.draggableProps}
+          ref={provided.innerRef}
+          bg="rgb(245,245,245)"
+          borderRadius="2xl"
+          boxShadow="sm"
+          width={320}
+          border="1px"
+          borderColor="rgb(230,230,230)"
+        >
+          <Box {...provided.dragHandleProps} pb="15px" width={320}>
+            <NameSettingsHolder subCompName="list" listId={listId} listName={listName} />
+          </Box>
+
+          <Box
+            maxHeight="360px"
+            minHeight="30px"
+            overflowY="auto"
+            sx={{
+              '&::-webkit-scrollbar': {
+                width: '5px',
+                borderRadius: '8px',
+                backgroundColor: `rgba(0, 0, 0, 0.05)`,
+              },
+              '&::-webkit-scrollbar-thumb': {
+                borderRadius: '8px',
+                backgroundColor: `rgb(179,179,179)`,
+              },
+            }}
+          >
+            <Droppable droppableId={list?.id.toString()!} direction="vertical" type="task">
+              {(provided) => (
+                <Box
+                  {...provided.droppableProps}
+                  ref={provided.innerRef}
+                  display="flex"
+                  flexDirection="column"
+                  justifyContent="center"
+                  alignItems="center"
+                  gap="20px"
+                  py="20px"
+                >
+                  {list?.cards?.map((item, index) => {
+                    return (
+                      <CardBox
+                        index={index}
+                        key={item.id}
+                        cardId={item.id}
+                        cardOrder={item.order!}
+                        listId={list.id}
+                        listName={list.title}
+                        boardName={boardName}
+                      />
+                    );
+                  })}
+                  {provided.placeholder}
+                </Box>
+              )}
+            </Droppable>
+          </Box>
+          <Box borderTop="1px" borderColor="rgb(230,230,230)">
+            <EditableAddButton item="card" listId={list?.id} />
+          </Box>
         </Box>
-      </Box>
-      <Box borderTop="1px" borderColor="rgb(230,230,230)">
-        <EditableAddButton item="card" listId={list?.id} />
-      </Box>
-    </Box>
+      )}
+    </Draggable>
   );
 };
 
