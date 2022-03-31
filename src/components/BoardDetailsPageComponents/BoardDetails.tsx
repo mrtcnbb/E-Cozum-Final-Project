8f342e78e7e23a45be3ef28c4ac4dc85ab85c0f1
Muratcan Baba <muratcanbaba1@gmail.com>
2022-03-31 09:46:22 +0300

    Dnd removed

--- src/components/BoardDetailsPageComponents/BoardDetails.tsx
+++ src/components/BoardDetailsPageComponents/BoardDetails.tsx
@@ -1,9 +1,7 @@
 import { Box } from '@chakra-ui/react';
 import { FC, useEffect } from 'react';
-import { DragDropContext, Droppable, DropResult } from 'react-beautiful-dnd';
 import { useParams } from 'react-router-dom';
 import { fetchBoard } from '../../features/boardSlice';
-import authRequest from '../../service/authRequest';
 import { useAppDispatch, useAppSelector } from '../../store';
 import Header from '../HomePageComponents/Header';
 import BoardDetailsHeader from './BoardDetailsHeader';
@@ -19,83 +17,28 @@ const BoardDetails: FC = () => {
     dispatch(fetchBoard(id!));
   }, [dispatch, id]);
 
-  const onDragEnd = (result: DropResult) => {
-    const { destination, source, draggableId, type } = result;
-
-    if (!destination) {
-      return;
-    }
-
-    if (destination.droppableId === source.droppableId && destination.index === source.index) {
-      return;
-    }
-
-    if (type === 'task') {
-      authRequest()
-        .put(`card/${draggableId}`, {
-          listId: Number(destination.droppableId),
-        })
-        .then(() => {
-          dispatch(fetchBoard(id!));
-          console.log('success');
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    }
-  };
-
   return (
-    <DragDropContext onDragEnd={onDragEnd}>
-      <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
-        <Header />
-        <BoardDetailsHeader />
-        <Box
-          display={'flex'}
-          flexDirection="row"
-          gap="20px"
-          justifyContent="flex-start"
-          alignItems="flex-start"
-          overflowY="auto"
-          flex={'1'}
-        >
-          <Droppable droppableId="all-columns" direction="horizontal" type="column">
-            {(provided) => (
-              <Box {...provided.droppableProps} ref={provided.innerRef}>
-                <Box
-                  p="4"
-                  sx={{
-                    display: 'flex',
-                    flexDirection: 'row',
-                    overflowY: 'auto',
-                    justifyContent: 'flex-start',
-                    alignItems: 'flex-start',
-                    gap: '20px',
-                  }}
-                >
-                  {board.data?.lists.map((item, index) => {
-                    return (
-                      <ListBox
-                        index={index}
-                        key={item.id}
-                        listId={item.id}
-                        listName={item.title}
-                        boardName={board.data?.title!}
-                        listOrder={item.order}
-                      />
-                    );
-                  })}
-                  {provided.placeholder}
-                </Box>
-              </Box>
-            )}
-          </Droppable>
-          <Box mt="16px">
-            <EditableAddButton item="list" />
-          </Box>
-        </Box>
+    <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+      <Header />
+      <BoardDetailsHeader />
+      <Box
+        p="4"
+        sx={{
+          flex: '1',
+          display: 'flex',
+          flexDirection: 'row',
+          overflowY: 'auto',
+          justifyContent: 'flex-start',
+          alignItems: 'flex-start',
+          gap: '20px',
+        }}
+      >
+        {board.data?.lists.map((item) => {
+          return <ListBox key={item.id} listId={item.id} listName={item.title} boardName={board.data?.title!} />;
+        })}
+        <EditableAddButton item="list" />
       </Box>
-    </DragDropContext>
+    </Box>
   );
 };
 
--- src/components/BoardDetailsPageComponents/CardBoxComponents/CardBox.tsx
+++ src/components/BoardDetailsPageComponents/CardBoxComponents/CardBox.tsx
@@ -2,31 +2,22 @@ import { Box, Text, Icon } from '@chakra-ui/react';
 import { BiCommentDetail, BiFile } from 'react-icons/bi';
 import CardMiniLabel from './CardMiniLabel';
 import CardCheckTag from './CardCheckTag';
-import { FC, useEffect, useState } from 'react';
-import { useAppDispatch, useAppSelector } from '../../../store';
+import { FC, useState } from 'react';
+import { useAppSelector } from '../../../store';
 import CardModal from '../CardModalComponents/CardModal';
 import React from 'react';
 import CardDateTag from './CardDateTag';
-import { Draggable } from 'react-beautiful-dnd';
-import authRequest from '../../../service/authRequest';
-import { fetchBoard } from '../../../features/boardSlice';
-import { useParams } from 'react-router-dom';
 
 interface CardBoxProps {
   cardId: number;
   listId: number;
   listName: string;
   boardName: string;
-  index?: number;
-  cardOrder: number;
+  ref?: any;
 }
 
-const CardBox: FC<CardBoxProps> = ({ cardId, listId, listName, boardName, index, cardOrder }) => {
+const CardBox: FC<CardBoxProps> = ({ cardId, listId, listName, boardName }) => {
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
-
-  const { id } = useParams();
-
-  const dispatch = useAppDispatch();
 
   const onOpen = () => {
     setIsModalOpen(!isModalOpen);
@@ -43,14 +34,6 @@ const CardBox: FC<CardBoxProps> = ({ cardId, listId, listName, boardName, index,
       .find((listItem) => listItem.id === listId)
       ?.cards.find((cardItem) => cardItem.id === cardId)
   );
-
-  useEffect(() => {
-    authRequest()
-      .put(`card/${cardId}`, { order: index })
-      .then(() => {
-        dispatch(fetchBoard(id!));
-      });
-  }, [cardId, dispatch, id, index]);
 
   const calculateTotalChecks = () => {
     let totalChecks = 0;
@@ -73,69 +56,55 @@ const CardBox: FC<CardBoxProps> = ({ cardId, listId, listName, boardName, index,
   };
 
   return (
-    <Draggable draggableId={card?.id.toString()!} index={cardOrder}>
-      {(provided) => (
-        <Box {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef}>
-          <Box
-            bg="rgb(250,250,250)"
-            _hover={{ cursor: 'pointer' }}
-            onClick={onOpen}
-            ref={divRef}
-            borderRadius="2xl"
-            boxShadow="sm"
-            width={286}
-            border="1px"
-            borderColor="rgb(230,230,230)"
-          >
-            <Box p="15px">
-              <Box display="flex" gap={2}>
-                {card?.labels.map((item) => {
-                  return <CardMiniLabel labelId={item.id} key={item.id} />;
-                })}
-              </Box>
-              <Text width={'90%'} overflow="hidden" textOverflow={'ellipsis'} whiteSpace="nowrap" my="18px">
-                {card?.title}
-              </Text>
-              <Box display="flex" justifyContent="start" gap="5px">
-                {card?.duedate && card?.duedate !== '2010-01-01' && <CardDateTag dueDate={card?.duedate} />}
-                {card?.checklists.length !== 0 && (
-                  <CardCheckTag
-                    totalCheckeds={() => calculateTotalCheckeds()}
-                    totalChecks={() => calculateTotalChecks()}
-                  />
-                )}
-              </Box>
-            </Box>
-            <Box
-              display="flex"
-              alignItems="center"
-              justifyContent="space-between"
-              height="47px"
-              p="18px"
-              borderTop="1px"
-              borderColor="rgb(230,230,230)"
-            >
-              <Box>{card?.description && <Icon boxSize={4} as={BiFile} />}</Box>
-              {card?.comments.length !== 0 && (
-                <Box display="flex" justifyContent="space-between" alignItems="center" width="30px">
-                  <Icon boxSize={4} mt="2px" as={BiCommentDetail} />
-                  <Text fontSize="xs" display="inline-block" lineHeight="0">
-                    {card?.comments.length}
-                  </Text>
-                </Box>
-              )}
-            </Box>
-            <CardModal
-              card={card!}
-              openModal={isModalOpen}
-              handleClose={onClose}
-              listName={listName}
-              boardName={boardName}
-            />
-          </Box>
+    <Box
+      bg="rgb(250,250,250)"
+      _hover={{ cursor: 'pointer' }}
+      onClick={onOpen}
+      ref={divRef}
+      borderRadius="2xl"
+      boxShadow="sm"
+      width={286}
+      border="1px"
+      borderColor="rgb(230,230,230)"
+    >
+      <Box p="15px">
+        <Box display="flex" gap={2}>
+          {card?.labels.map((item) => {
+            return <CardMiniLabel labelId={item.id} key={item.id} />;
+          })}
         </Box>
-      )}
-    </Draggable>
+        <Text width={'90%'} overflow="hidden" textOverflow={'ellipsis'} whiteSpace="nowrap" my="18px">
+          {card?.title}
+        </Text>
+        <Box display="flex" justifyContent="start" gap="5px">
+          {card?.duedate && card?.duedate !== '2010-01-01' && <CardDateTag dueDate={card?.duedate} />}
+          {card?.checklists.length !== 0 && (
+            <CardCheckTag totalCheckeds={() => calculateTotalCheckeds()} totalChecks={() => calculateTotalChecks()} />
+          )}
+        </Box>
+      </Box>
+
+      <Box
+        display="flex"
+        alignItems="center"
+        justifyContent="space-between"
+        height="47px"
+        p="18px"
+        borderTop="1px"
+        borderColor="rgb(230,230,230)"
+      >
+        <Box>{card?.description && <Icon boxSize={4} as={BiFile} />}</Box>
+        {card?.comments.length !== 0 && (
+          <Box display="flex" justifyContent="space-between" alignItems="center" width="30px">
+            <Icon boxSize={4} mt="2px" as={BiCommentDetail} />
+            <Text fontSize="xs" display="inline-block" lineHeight="0">
+              {card?.comments.length}
+            </Text>
+          </Box>
+        )}
+      </Box>
+      <CardModal card={card!} openModal={isModalOpen} handleClose={onClose} listName={listName} boardName={boardName} />
+    </Box>
   );
 };
 
